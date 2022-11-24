cad3eae7b331ccd73c103a5a5f442f79ef5bda58
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-11-24 18:40:42 -0400

    feat: create view_sets

---
+++ real_state_agency/api/view_sets.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from real_state_agency.api import serializers
+from properties import models
+
+
+class PropertiesViewSet(viewsets.ModelViewSet):
+    queryset = models.Property.objects.all()
+    serializer_class = serializers.PropertiesSerializer
+
+
+class AdvertsViewSet(viewsets.ModelViewSet):
+    queryset = models.Advert.objects.all()
+    serializer_class = serializers.AdvertsSerializer
+
+
+class BookingsViewSet(viewsets.ModelViewSet):
+    queryset = models.Booking.objects.all()
+    serializer_class = serializers.BookingsSerializer
