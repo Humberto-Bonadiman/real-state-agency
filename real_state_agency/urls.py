47b638918a8ee026817953ac7c25f0587b36ca19
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-11-29 01:07:12 -0400

    refactor: remove comments

--- real_state_agency/urls.py
+++ real_state_agency/urls.py
@@ -1,38 +1,5 @@
-"""real_state_agency URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, include
-# from rest_framework import routers
-# from properties.urls import route
 
-
-""" route = routers.DefaultRouter()
-route.register(r'property', views.property_list, basename='Properties')
-route.register(r'advert', views.advert_list, basename='Adverts')
-route.register(r'booking', views.booking_list, basename='Booking')
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(
-        route.urls,
-    ))
-] """
 urlpatterns = [
     path('', include('properties.urls')),
 ]
-""" urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(route.urls))
-] """
